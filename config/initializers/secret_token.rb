9c358f3c4d2fafe1e42675faac698691ec18db7b
Jerzon Hernandez <joel.hz24@gmail.com>
2012-06-18 13:48:04 -0600

    Init: Configurado con PostgreSQL

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RosalilaSite::Application.config.secret_token = '046044ad1d857a5618d666b116d1e59b2c44cb279762911403fdd8c4da8fdd389d82f64f7d273fa1034996ced4205fa6b6b4811caba76c24d26534f7fa513cad'
