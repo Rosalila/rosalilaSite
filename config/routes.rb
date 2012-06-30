2dd952b5b831d8e87c16dd23cf1f380ae71032bb
Jerzon Hernandez <joel.hz24@gmail.com>
2012-06-30 15:27:13 -0600

    log_in && log_out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 RosalilaSite::Application.routes.draw do
+  
+	get "log_in" => "sessions#new", :as => "log_in"
+	get "log_out" => "sessions#destroy", :as => "log_out"
+
   get "sign_up" => "users#new", :as => "sign_up"
 	root :to => "users#new"
 	resources :users
+	resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
